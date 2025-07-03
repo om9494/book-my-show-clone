47024b6b89334119b9841c5d4c3be381eaf19241
Anushka Jadhav <150259274+anushkasj06@users.noreply.github.com>
2025-07-03 11:04:18 +0530

    Update ShowController.java

--- src/main/java/com/bookmyshow/Controllers/ShowController.java
+++ src/main/java/com/bookmyshow/Controllers/ShowController.java
@@ -12,12 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.sql.Time;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/shows")
